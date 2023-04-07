620d0997762482746b5128831ac9850ac2fbb5c7
Kevin Pedro <kpedro88@gmail.com>
2023-04-07 16:24:29 -0500

    use patched version of delphes (& make script rerunnable)

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,19 +3,24 @@
 # CMSSW
 CMSSW_VERSION=CMSSW_10_6_19
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-scramv1 project CMSSW ${CMSSW_VERSION}
+if [ ! -d $CMSSW_VERSION ]; then
+	scramv1 project CMSSW ${CMSSW_VERSION}
+fi
 pushd ${CMSSW_VERSION}/src
 eval `scramv1 runtime -sh`
 popd
 
 # Delphes
+DELPHES_VERSION=Delphes-3.4.2
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-wget http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.4.2.tar.gz
-tar -zxf Delphes-3.4.2.tar.gz
-cd Delphes-3.4.2
+if [ ! -d $DELPHES_VERSION ]; then
+	git clone https://github.com/kpedro88/delphes -b 3.4.2_svj $DELPHES_VERSION
+fi
+pushd $DELPHES_VERSION
 cp ../delphes_card_CMS.tcl cards
 sed -i '/^CXXFLAGS/ s/c++0x/c++17/' Makefile
 make -j 4
+popd
 
 # Gridpack
-tar czf gridpack_delphes_3.4.2.tgz Delphes-3.4.2
+tar --exclude-vcs -czf gridpack_delphes_3.4.2.tgz $DELPHES_VERSION
